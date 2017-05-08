7fcf5850064f1d73135563237be7db7343a20789
peloton <j.peloton@sussex.ac.uk>
2017-05-08 17:29:40 +0100

    Clean variable name

--- covariances.py
+++ covariances.py
@@ -3,7 +3,7 @@ import healpy as hp
 import numpy as np
 import os
 
-def generate_covariances(m1, out):
+def generate_covariances(m1, inst):
     """
     Create a weight map using the smaller eigenvalue of the polarization matrix
     The resulting covariances are saved on the disk.
@@ -11,19 +11,21 @@ def generate_covariances(m1, out):
     Parameters
     ----------
     * m1: object, contain the observations
-    * out: object, contain the input parameters from the ini file
+    * inst: object, contain the input parameters from the ini file
 
     """
     nside = m1.mapinfo.nside
     obspix = m1.mapinfo.obspix
 
     Pw = util_CMB.partial2full(
-        util_CMB.qu_weight_mineig(m1.cc, m1.cs, m1.ss, epsilon=0.), obspix, nside)
+        util_CMB.qu_weight_mineig(
+            m1.cc, m1.cs, m1.ss, epsilon=0.), obspix, nside)
     Iw = util_CMB.partial2full(m1.w, obspix, nside)
 
     path = os.path.join(
-        out.outpath_masks,
-        'IQU_nside%d_%s_weights_freq%s.fits'%(nside, out.name, out.frequency))
+        inst.outpath_masks,
+        'IQU_nside%d_%s_weights_freq%s.fits' % (
+            nside, inst.out_name, inst.frequency))
 
     util_CMB.write_map(
             path,
