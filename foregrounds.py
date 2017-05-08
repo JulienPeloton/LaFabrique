045561398bbff6eac21ea28363a2fcafa97d27ef
peloton <j.peloton@sussex.ac.uk>
2017-05-08 17:31:39 +0100

    Foregrounds ini synchronized with instrument one

--- foregrounds.py
+++ foregrounds.py
@@ -14,75 +14,107 @@ import pysm_freefree
 import util_CMB
 
 from pysm import config2list
-from pysm import write_output_single
 
-def generate_foregrounds(config_file):
+def generate_foregrounds(config_file, env=None):
     """
     Adapted from the main of the PySM
     """
-    ## Get the output directory and save the configuration file.
+    ## TODO: transfer things from instrument to foregrounds!!
+    
+    if env is None:
+        env.verbose = False
+        env.plot = False
+        env.out_name = 'temp'
+        env.outpath_noise = './'
+        env.outpath_masks = './'
+
+    ## Get the default parameters
     Config = ConfigParser.ConfigParser()
     Config.read(config_file)
-    out = util_CMB.normalise_foreground_parser(
+    foregrounds = util_CMB.normalise_foreground_parser(
         Config._sections['GlobalParameters'])
-    if not os.path.exists(out.output_dir):
-        os.makedirs(out.output_dir)
-    path = os.path.join(out.output_dir, out.output_prefix + 'main_config.ini')
+
+    ## Share few parameters between objects
+    if env is not None:
+        ## NOTE: the trailing '/' is required.
+        foregrounds.output_dir = env.outpath_foregrounds + '/'
+        foregrounds.output_prefix = env.out_name + '_'
+        Config.set(
+            'GlobalParameters', 'output_dir', foregrounds.output_dir)
+        Config.set(
+            'GlobalParameters', 'output_prefix', foregrounds.output_prefix)
+    path = os.path.join(
+        foregrounds.output_dir,
+        foregrounds.output_prefix + 'main_config.ini')
+
+    ## Create output folder
+    if not os.path.exists(foregrounds.output_dir):
+        os.makedirs(foregrounds.output_dir)
+
+    ## Save the modified ini file, and reload it
     with open(path, 'w') as configfile:
         Config.write(configfile)
 
-    if out.debug is True:
+    ## Re-open the file with modified options if necessary
+    config_file = path
+    Config.read(config_file)
+    foregrounds = util_CMB.normalise_foreground_parser(
+        Config._sections['GlobalParameters'])
+
+    if foregrounds.debug is True:
         ## Print information about the run:
         print '----------------------------------------------------- \n'
-        print ''.join("%s: %s \n" % item for item in vars(out).items())
+        print ''.join("%s: %s \n" % item for item in vars(foregrounds).items())
         print '-----------------------------------------------------'
 
-    sky = np.zeros([3, len(out.output_frequency), hp.nside2npix(out.nside)])
+    sky = np.zeros(
+        [3, len(foregrounds.output_frequency), hp.nside2npix(
+            foregrounds.nside)])
     print '----------------------------------------------------- \n'
     ## Create synchrotron, dust, AME,
     ## and cmb maps at output frequencies then add noise.
-    if 'synchrotron' in out.components:
+    if 'synchrotron' in foregrounds.components:
         sky = sky + pysm_synchrotron.main(config_file)
 
-    if 'thermaldust' in out.components:
+    if 'thermaldust' in foregrounds.components:
         sky = sky + pysm_thermaldust.main(config_file)
 
-    if 'spinningdust' in out.components:
+    if 'spinningdust' in foregrounds.components:
         sky = sky + pysm_spinningdust.main(config_file)
 
-    if 'freefree' in out.components:
+    if 'freefree' in foregrounds.components:
         sky = sky + pysm_freefree.main(config_file)
 
-    if 'cmb' in out.components:
+    if 'cmb' in foregrounds.components:
         sky = sky + pysm_cmb.main(config_file)
 
-    if out.smoothing:
+    if foregrounds.smoothing:
         print 'Smoothing output maps.'
         print '----------------------------------------------------- \n'
-        for i in xrange(len(out.output_frequency)):
+        for i in xrange(len(foregrounds.output_frequency)):
             sky[:, i, :] = hp.smoothing(
                 sky[:, i, :],
-                fwhm=(np.pi / 180. / 60.) * out.fwhm[i],
+                fwhm=(np.pi / 180. / 60.) * foregrounds.fwhm[i],
                 verbose=False)
 
-    if out.instrument_noise is True:
+    if foregrounds.instrument_noise is True:
         sky = sky + pysm_noise.instrument_noise(config_file)
 
     comps = str()
 
-    if out.instrument_noise:
-        out.components.append('noise')
+    if foregrounds.instrument_noise:
+        foregrounds.components.append('noise')
 
     sky = np.swapaxes(sky, 0, 1)
-    for i in xrange(len(out.output_frequency)):
-        if out.output_coordinate_system:
+    for i in xrange(len(foregrounds.output_frequency)):
+        if foregrounds.output_coordinate_system:
             sky[i, ...] = util_CMB.rot_sky_map(
                 sky[i, ...],
-                coord=['G', out.output_coordinate_system])
-        extra_header = config2list(Config, out, i)
+                coord=['G', foregrounds.output_coordinate_system])
+        extra_header = config2list(Config, foregrounds, i)
         util_CMB.write_output_single(
             sky[i, ...],
-            out,
+            foregrounds,
             Config,
             i,
             extra_header=extra_header)
